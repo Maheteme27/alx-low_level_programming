0be21aa5634d1f0384dfeab78b09afefb96ff8bf
Maheteme27 <maheteme380@gmail.com>
2022-12-09 05:40:03 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the size of various types based on
+ * the computer it is compiled and run on..
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	  write(2,
+			          "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+				          59);
+	    return (1);
+}
