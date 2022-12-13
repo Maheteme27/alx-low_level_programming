68e217d4fd9d653db80b7b61df5834efc8235aef
Maheteme27 <maheteme380@gmail.com>
2022-12-13 04:16:34 -0800

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,9 +4,11 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
+
  */
 int main(void)
 {
+
         _putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -18,3 +20,4 @@ int main(void)
 	_putchar('\n');
 
 	return(0);
+}
