843b8cf028473f33bb5e468132c7d1f44ce5a440
Maheteme27 <maheteme380@gmail.com>
2023-01-12 09:55:44 -0800

    update

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,22 @@
+main.h
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+0
+#include <stdlib.h>
+/**
+  * malloc_checked - allocates memory using malloc
+  * @b: size of buffer
+  * Return: pointer to buffer
+  */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
